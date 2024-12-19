60c4529312535bb624fe21af5d4276011f5c96fa
LPavlovicCCT <ptb2024001@student.cct.ie>
2024-12-19 16:31:54 +0000

    DiagonalSum begginig diagonal sum, i cant print out array directly using Arrays.toString() i will serac for a colution that can hjandle 2D arrays

--- DiagonalSumOfMatrix.java
+++ DiagonalSumOfMatrix.java
@@ -1,7 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 class DiagonalSumOfMatrix
 {
     public static void main(String[]args)
     {
         //calculate sum of main diagonal and secondary diagonal from a square matrix from user input
+        Scanner reader = new Scanner(System.in);
+
+        //ask for lenght and members
+        System.out.print("What is the size of the matrix? (nxn) n=");
+        int n = reader.nextInt();
+        //make array 
+        int[][] array = new int[n][n];
+        
+        //add members
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print("["+i+"]["+j+"]=");
+                array[i][j] = reader.nextInt();
+            }
+            System.out.println();
+        }
+
+        System.out.println(Arrays.toString(array));
+
+
+        reader.close();
     }
 };
